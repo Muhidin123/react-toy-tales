1ff2d8dc2bd0c8799ad50201ff298154f54ce4e5
Muhidin <muhidinhukic1@gmail.com>
2021-01-16 18:29:29 -0600

    slight change to ToyContainer

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,25 +1,43 @@
 import { render } from "@testing-library/react";
 import ToyCard from "./ToyCard";
-import React, { Component } from "react";
+import React from "react";
 
-export default class ToyContainer extends Component {
-  constructor(props) {
-    super(props);
-  }
+// OPTION 1 to do it:
 
-  render() {
-    return (
-      <div id="toy-collection">
-        {this.props.toys.map((toy) => {
-          return (
-            <ToyCard
-              key={toy.id}
-              toy={toy}
-              handleDelete={this.props.handleDelete}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+// export default class ToyContainer extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return (
+//       <div id="toy-collection">
+//         {this.props.toys.map((toy) => {
+//           return (
+//             <ToyCard
+//               key={toy.id}
+//               toy={toy}
+//               handleDelete={this.props.handleDelete}
+//             />
+//           );
+//         })}
+//       </div>
+//     );
+//   }
+// }
+
+// OPTION 2 to do it
+
+const ToyContainer = (props) => {
+  return (
+    <div id="toy-collection">
+      {props.toys.map((toy) => {
+        return (
+          <ToyCard key={toy.id} toy={toy} handleDelete={props.handleDelete} />
+        );
+      })}
+    </div>
+  );
+};
+
+export default ToyContainer;
